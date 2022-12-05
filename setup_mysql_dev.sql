72fb0a84d3237b2ec7031e9041fa11c48ff9001b
OttoNyongole <nyongole.otto@gmail.com>
2022-12-06 00:07:22 +0300

    added sql file for development

---
+++ setup_mysql_dev.sql
@@ -0,0 +1,15 @@
+--A script that prepares a MySQL server --
+-- Create db 'hbnb_dev_db' --
+CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
+
+-- Add new user 'hbnb_dev' in localhost --
+-- User 'hbnb_dev' password should be 'hbnb_dev_pwd' --
+CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
+
+-- User 'hbnb_dev' should have all privileges on database performance_schema
+GRANT ALL PRIVILEGES ON *.* TO 'hbnb_dev'@'localhost';
+
+-- User 'hbnb_dev' should have SELECT privileges on database 'performance_schema'
+GRANT SELECT ON performance_schema.table TO 'hbnb_dev'@'localhost';
+
+FLUSH PRIVILEGES;
