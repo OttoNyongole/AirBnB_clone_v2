86ace18c84bef52dce36b3296f53935f0d4624d1
OttoNyongole <nyongole.otto@gmail.com>
2022-12-06 10:30:59 +0300

    modified setp_mysql_dev.sql

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,9 +1,9 @@
---A script that prepares a MySQL server --
--- Create db 'hbnb_dev_db' --
+-- A script that prepares a MySQL server 
+-- Create db 'hbnb_dev_db' 
 CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
 
--- Add new user 'hbnb_dev' in localhost --
--- User 'hbnb_dev' password should be 'hbnb_dev_pwd' --
+-- Add new user 'hbnb_dev' in localhost 
+-- User 'hbnb_dev' password should be 'hbnb_test_pwd'
 CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
 
 -- User 'hbnb_dev' should have all privileges on database performance_schema
