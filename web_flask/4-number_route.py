49671de401566f863d5c1dcccf1c1c5aa1064a55
OttoNyongole <nyongole.otto@gmail.com>
2023-01-14 12:16:49 +0300

    correction on last line on run method

--- web_flask/4-number_route.py
+++ web_flask/4-number_route.py
@@ -1,42 +1,40 @@
 #!/usr/bin/python3
-"""
-starts a Flask web application
-"""
-
+"""Flask"""
 from flask import Flask
 app = Flask(__name__)
 
 
 @app.route('/', strict_slashes=False)
-def index():
-    """returns Hello HBNB!"""
-    return 'Hello HBNB!'
+def hello_route():
+    """hello_route"""
+    return "Hello HBNB!"
 
 
 @app.route('/hbnb', strict_slashes=False)
 def hbnb():
-    """returns HBNB!"""
-    return 'HBNB!'
+    """hbnb"""
+    return "HBNB"
 
 
 @app.route('/c/<text>', strict_slashes=False)
-def cisfun(text):
-    """display "C " followed by the value of the text variable"""
-    return 'C ' + text.replace('_', ' ')
+def c_route(text):
+    """c_route"""
+    return "C {}".format(text.replace("_", " "))
 
 
 @app.route('/python', strict_slashes=False)
 @app.route('/python/<text>', strict_slashes=False)
-def pythoniscool(text='is cool'):
-    """display "python " followed by the value of the text variable"""
-    return 'python ' + text.replace('_', ' ')
+def python_route(text="is cool"):
+    """python_route"""
+    return "Python {}".format(text.replace("_", " "))
 
 
 @app.route('/number/<int:n>', strict_slashes=False)
-def isanumber(n):
-    """display "n is a number" only if n is an integer"""
-    return "{:d} is a number".format(n)
+def number_route(n):
+    """number_route"""
+    if isinstance(n, int):
+        return "{} is a number".format(n)
 
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port='5000')
+if __name__ == "__main__":
+    app.run()
